96533d6f17278d7a228796d09d2b6eac70144a84
Ntsika Calvin Ngilane <workNgilane@gmail.com>
2025-08-29 20:08:40 +0200

    more cv fix for deployment

--- components/cv-viewer.tsx
+++ components/cv-viewer.tsx
@@ -4,8 +4,8 @@ import { useState } from "react"
 import { Download, ExternalLink, FileText, Loader2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Document, Page, pdfjs } from 'react-pdf'
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
-import 'react-pdf/dist/esm/Page/TextLayer.css'
+import 'react-pdf/dist/Page/AnnotationLayer.css'
+import 'react-pdf/dist/Page/TextLayer.css'
 
 // Configure PDF.js worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
